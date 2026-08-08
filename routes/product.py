a9196a7e3e66f8278a62905a3bf504b8fe507986
Harsh Chauhan <forskills705@gmail.com>
2026-08-08 17:37:25 +0530

    redis caching in product

--- routes/product.py
+++ routes/product.py
@@ -5,9 +5,12 @@ from schemas.product import ProductCreate,ProductUpdate
 from models.product import Product
 
 router = APIRouter(prefix="/products",tags=["Products"])
+import json
+from redis_database import redis_client 
+
 
 @router.post("/")
-def create_product(product: ProductCreate,
+async def create_product(product: ProductCreate,
     db: Session = Depends(get_db)):
 
     new_product = Product(**product.model_dump())
@@ -16,29 +19,75 @@ def create_product(product: ProductCreate,
     db.commit()
     db.refresh(new_product)
 
+    await redis_client.delete("products")
     return {
         "message": "Product Created",
         "product": new_product
     }
 
-@router.get("/")
-def get_products(db: Session = Depends(get_db)):
+@router.get("/products")
+async def get_products(db: Session = Depends(get_db)):
+
+    cached = await redis_client.get("products")
+
+    if cached:
+        print("Cache Hit")
+        return json.loads(cached)
+
+    print("Cache Miss")
 
     products = db.query(Product).all()
-    return products
 
-@router.get("/{product_id}")
-def get_product(product_id: int,db: Session = Depends(get_db)):
+    result = [
+        {
+            "id": p.id,
+            "name": p.name,
+            "price": p.price,
+            "category": p.category
+        }
+        for p in products
+    ]
 
-    product = db.query(Product).filter(Product.id == product_id).first()
+    await redis_client.set(
+        "products",
+        json.dumps(result),
+        ex=300
+    )
+
+    return result
+
+@router.get("/products/{id}")
+async def get_product(id:int, db:Session=Depends(get_db)):
+
+    key = f"product:{id}"
+
+    cached = await redis_client.get(key)
+
+    if cached:
+        return json.loads(cached)
+
+    product = db.query(Product).filter(Product.id==id).first()
 
     if not product:
-        return {"message": "Product Not Found"}
+        raise HTTPException(404,"Product not found")
 
-    return product
+    result = {
+        "id":product.id,
+        "name":product.name,
+        "price":product.price,
+        "category":product.category
+    }
+
+    await redis_client.set(
+        key,
+        json.dumps(result),
+        ex=300
+    )
+
+    return result
 
 @router.put("/{product_id}")
-def update_product(product_id: int,
+async def update_product(product_id: int,
     updated_product: ProductUpdate,
     db: Session = Depends(get_db)):
 
@@ -57,13 +106,16 @@ def update_product(product_id: int,
     db.commit()
     db.refresh(product)
 
+    await redis_client.delete("products")
+    await redis_client.delete(f"product:{product_id}")
+
     return {
         "message": "Product Updated Successfully",
         "product": product
     }
 
 @router.delete("/{product_id}")
-def delete_product(product_id:int,
+async def delete_product(product_id:int,
     db:Session=Depends(get_db)):
 
     product=db.query(Product).filter(
@@ -74,6 +126,9 @@ def delete_product(product_id:int,
         raise HTTPException(status_code=404,
             detail="Product Not Found"
         )
+
+    await redis_client.delete("products")
+    await redis_client.delete(f"product:{product_id}")
 
     db.delete(product)
     db.commit()
